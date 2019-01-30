ed9378e12df207ea002de8c1da4364f8a68b8f47
Space <Space0726@github.com>
2019-01-30 23:55:09 +0900

    Solved programmers_exhaustiveSearch_2

--- Programmers/exhaustiveSearch/programmers_exhaustiveSearch_2.py
+++ Programmers/exhaustiveSearch/programmers_exhaustiveSearch_2.py
@@ -1,19 +1,24 @@
-from itertools import chain, combinations
-def powerset(iterable):
-	xs = list(iterable)
-	return chain.from_iterable(combinations(xs, n) for n in range(len(xs)+1))
+from itertools import permutations
+'''
+numbers1 = "17"
+numbers2 = "011"
+numbers3 = "5234901"
+'''
+def subset(li):
+	result = []
+	for i in range(1, len(li) + 1):
+		result.extend(permutations(li,i))
+	return result
 
 def isPrime(n):
-	if n < 1:
+	if n < 2:
 		return False
 	for i in range(2, n):
 		if n % i == 0:
 			return False
 	return True
 
-numbers1 = "17"
-numbers2 = "011"
-new = list(map(lambda x: ''.join([a for a in x]), powerset(numbers2))[1:])
-new = list(set(map(int, new)))
-print(sum(list(map(lambda x: isPrime(int(x)), new))))
-filter(lambda x: x, new)
+def solution(numbers):
+	new = list(map(lambda x: ''.join([a for a in x]), subset(numbers)))
+	new = list(set(map(int, new)))
+	return sum(list(map(lambda x: isPrime(int(x)), new)))
