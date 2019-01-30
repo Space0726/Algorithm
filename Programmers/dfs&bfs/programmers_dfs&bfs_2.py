39ddcb4fab6dfb4523848b20da09bb239e8ad1ea
Space <Space0726@github.com>
2019-01-31 03:09:55 +0900

    Solved programmers_dfs&bfs_2

---
+++ Programmers/dfs&bfs/programmers_dfs&bfs_2.py
@@ -0,0 +1,28 @@
+def dfs(graph, start):
+	visited = []
+	stack = [start]
+	while stack:
+		n = stack.pop()
+		if n not in visited:
+			visited.append(n)
+			stack += set(graph[n]) - set(visited)
+	return visited
+
+def solution(n, computers):
+	linked = []
+	num = 0
+	graph = dict(enumerate(computers))
+	for i in range(n):
+		graph[i] = [idx for idx, v in enumerate(computers[i]) if v == 1 and idx != i]	
+	for i in range(n):
+		if i in linked:
+			continue
+		linked.extend(dfs(graph, i))
+		num += 1
+	return num
+
+'''
+computers = [[1,1,0],[1,1,1],[0,1,1]]
+n = 3
+print(solution(n, computers))
+'''
