f0fd8ceba0e0770112b2f1491fbdbd6a603385fb
Space <Space0726@github.com>
2019-01-31 01:59:32 +0900

    Solved programmers_dfs&bfs_1

---
+++ Programmers/dfs&bfs/programmers_dfs&bfs_1.py
@@ -0,0 +1,25 @@
+'''
+result = 0
+
+def dfs(numbers, target, k):
+	if k == len(numbers):
+		if sum(numbers) == target:
+			global result
+			result += 1
+	else:
+		numbers[k] *= 1
+		dfs(numbers, target, k+1)
+		numbers[k] *= -1
+		dfs(numbers, target, k+1)
+
+def first_solution(numbers, target):
+	dfs(n, target, 0)
+	return result
+'''
+def solution(numbers, target):
+	if not numbers and target == 0:
+		return 1
+	elif not numbers:
+		return 0
+	else:
+		return solution(numbers[1:], target - numbers[0]) + solution(numbers[1:], target + numbers[0])
