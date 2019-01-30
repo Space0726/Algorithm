9a17511d53a9909b0d2cdf08153fdd8d378db188
Space <Space0726@github.com>
2019-01-31 03:46:30 +0900

    Solved programmers_stack&queue_1

---
+++ Programmers/stack&queue/programmers_stack&queue_1.py
@@ -0,0 +1,18 @@
+def solution(arrangement):
+	count = 0
+	result = 0
+	lv = ''
+	listArr = list(arrangement)
+	while listArr:
+		v = listArr.pop()
+		if v == ')':
+			count += 1
+		else:
+			if v != lv:
+				count -= 1
+				result += count
+			else:
+				result += 1
+				count -= 1
+		lv = v
+	return result
