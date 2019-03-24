6873fcabf705824feef984ed676a73f715ae16a8
Space <Space@github.com>
2019-03-24 19:41:16 +0900

    Remove 1545.py and 2072.py that not in D1 directory

--- SW_Expert_Academy/1545.py
+++
@@ -1,6 +0,0 @@
-''' Original answer
-for i in range(int(input()),-1,-1):
-    print(i,end=' ')
-'''
-
-print(*range(int(input()),-1,-1))
--- SW_Expert_Academy/2072.py
+++
@@ -1,2 +0,0 @@
-for i in range(int(input())):
-    print(f'#{i+1} {sum(filter(lambda x: x%2, map(int, input().split())))}')
