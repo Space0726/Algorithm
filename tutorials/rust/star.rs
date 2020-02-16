037862ac087a656da08af2a7df49faf22bc1b538
Space0726 <space.sjwoo@gmail.com>
2020-02-17 01:10:20 +0900

    Modify match to if let

--- tutorials/rust/star.rs
+++ tutorials/rust/star.rs
@@ -6,7 +6,7 @@ fn square(num: u32) -> u32 {
 
 fn print_stars(num: u32) {
     for i in 1..num + 1 {
-        for j in 0..i {
+        for _j in 0..i {
             print!("*");
         }
         println!();
@@ -17,10 +17,10 @@ fn main() {
     let mut string = String::new();
     println!("Input number: ");
     io::stdin().read_line(&mut string).expect("Error!");
-    let mut num: u32 = match string.trim().parse(){
-        Ok(n) => n,
-        Err(_) => 0,
-    };
+    let mut num: u32 = 0;
+    if let Ok(n) = string.trim().parse() {
+        num = n;
+    }
     num = square(num);
     println!("Squared: {}", num);
     print_stars(num);
