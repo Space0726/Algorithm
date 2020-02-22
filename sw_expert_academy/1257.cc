a002731f43c74f64a2e7a6af7b40e5ae4da26cf3
Space0726 <space.sjwoo@gmail.com>
2020-02-22 16:41:25 +0900

    Adds SWEA 1257

---
+++ sw_expert_academy/1257.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int k;
+set<string> strs;
+
+int main() {
+    int t, cnt;
+    string tmp;
+    scanf("%d", &t);
+    for (int test_case = 1; test_case <= t; ++test_case) {
+        cin >> k >> tmp;
+        strs = set<string>();
+        for (int j = 1; j <= tmp.size(); ++j)
+            for (int i = 0; i < tmp.size() - j + 1; ++i)
+                strs.insert(tmp.substr(i, j));
+        cnt = 0;
+        cout << "#" << test_case << " ";
+        if (k > strs.size())
+            cout << "none\n";
+        else
+            for (auto it = strs.begin(); it != strs.end(); ++it)
+                if (++cnt == k)
+                    cout << *it << '\n';
+    }
+    return 0;
+}
