14367d754a28902bf333351cd7f7270f111f1a1d
Space0726 <space.sjwoo@gmail.com>
2020-02-22 23:58:48 +0900

    Modify sort.cpp

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -10,77 +10,76 @@ void insertion_sort(vector<int> &v);
 void quick_sort(vector<int> &v, int a, int b);
 
 int main(void) {
-	vector<int> numbers = {4,2,6,7,8,3,5,1,9,0};
+    vector<int> numbers = {4,2,6,7,8,3,5,1,9,0};
     quick_sort(numbers, 0, numbers.size() - 1);
-	return 0;
+    return 0;
 }
 
 void print_vector(vector<int> &v) {
-	for (int value: v)
-		cout << value << ' ';
-	cout << endl;
+    for (int value: v)
+        cout << value << ' ';
+    cout << endl;
 }
 
 // Best: O(n), Worst: O(n^2)
 void bubble_sort(vector<int> &v) {
-	int temp;
-	print_vector(v);
-	cout << endl;
+    int temp;
+    print_vector(v);
+    cout << endl;
 
-	for (int i = 0; i < v.size(); i++) {
-		for (int j = 0; j < v.size() - (i+1); j++) {
-			if (v[j] > v[j+1]) {
-				temp = v[j];
-				v[j] = v[j+1];
-				v[j+1] = temp;
-			}
-			print_vector(v);
-		}
-		cout << endl;
-	}
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v.size() - (i+1); j++) {
+            if (v[j] > v[j+1]) {
+                temp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = temp;
+            }
+            print_vector(v);
+        }
+        cout << endl;
+    }
 }
 
 // Best: O(n^2), Worst: O(n^2)
 void selection_sort(vector<int> &v) {
-	int temp;
-	int min;
-	print_vector(v);
-	cout << endl;
+    int temp;
+    int min;
+    print_vector(v);
+    cout << endl;
 
-	for (int i = 0; i < v.size() - 1; i++) {
-		min = i;
-		for (int j = i + 1; j < v.size(); j++)
-			if (v[min] > v[j])
-				min = j;
-		temp = v[i];
-		v[i] = v[min];
-		v[min] = temp;
-		print_vector(v);
-		cout << endl;
-	}
+    for (int i = 0; i < v.size() - 1; i++) {
+        min = i;
+        for (int j = i + 1; j < v.size(); j++)
+            if (v[min] > v[j])
+                min = j;
+        temp = v[i];
+        v[i] = v[min];
+        v[min] = temp;
+        print_vector(v);
+        cout << endl;
+    }
 }
 
 // Best: O(n), Worst: O(n^2)
 void insertion_sort(vector<int> &v) {
-	int temp;
-	print_vector(v);
-	cout << endl;
+    int temp;
+    print_vector(v);
+    cout << endl;
 
-	for (int i = 1; i < v.size(); i++) {
-		for (int j = i; j > 0; j--) {
-			if (v[j-1] > v[j]) {
-				temp = v[j-1];
-				v[j-1] = v[j];
-				v[j] = temp;
-				print_vector(v);
-			}
-			else {
-				print_vector(v);
-				break;
-			}
-		}
-		cout << endl;
-	}
+    for (int i = 1; i < v.size(); i++) {
+        for (int j = i; j > 0; j--) {
+            if (v[j-1] > v[j]) {
+                temp = v[j-1];
+                v[j-1] = v[j];
+                v[j] = temp;
+                print_vector(v);
+            } else {
+                print_vector(v);
+                break;
+            }
+        }
+        cout << endl;
+    }
 }
 
 void swap(vector<int> &v, int a, int b) {
@@ -89,22 +88,19 @@ void swap(vector<int> &v, int a, int b) {
     v[b] = t;
 }
 
-// int partition(vector<int> &v, int left, int right) {
-    // int mid = (left + right) / 2;
-    // swap(v, left, mid);
+// For quick sort partition
+int partition(vector<int> &v, int left, int right) {
+    swap(v, left, (left + right) / 2);
+    int i = left, j = right + 1;
 
-    // int pivot = v[left];
-    // int i = left - 1, j = right + 1;
-
-    // while (i < j) {
-        // while (pivot < v[--j]);
-        // while (i < j && pivot >= v[++i]);
-        // if (i < j) swap(v, i, j);
-    // }
-    // v[left] = v[i];
-    // v[i] = pivot;
-    // return i;
-// }
+    while (i < j) {
+        while (v[left] < v[--j]);
+        while (i < j && v[left] >= v[++i]);
+        if (i < j) swap(v, i, j);
+    }
+    swap(v, left, i);
+    return i;
+}
 
 // Best: O(nlogn), Worst: O(n^2)
 void quick_sort(vector<int> &v, int left, int right) {
