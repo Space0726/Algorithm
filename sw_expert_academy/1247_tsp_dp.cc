75f9b231308df63c8cbeefac96c996896fa87039
Space0726 <space.sjwoo@gmail.com>
2020-03-22 20:14:53 +0900

    Adds TSP dp solution

--- sw_expert_academy/1247_tsp_dp.cc
+++ sw_expert_academy/1247_tsp_dp.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int N, w[12][12], coord[12][2], dist[10][1 << 10];
+int N, w[12][12], coord[12][2], dp[12][1 << 10];
 
 inline int abs(int a) { return a < 0 ? -a : a; }
 
@@ -10,21 +10,19 @@ inline int distance(int sx, int sy, int ex, int ey) {
     return abs(sx - ex) + abs(sy - ey);
 }
 
-inline int min(int a, int b) { return a < b ? a : b; }
-
-int travel() {
-    int i, j, k;
-    for (i = 1; i <= N; ++i)
-        dist[i][i-1] = w[i][0];
-    for (k = 1; k <= N; ++k) {
-        subset(k);
-        dist[0][] = min();
+int travel(int node, int visited) {
+    if (visited == ((1 << N) - 1)) return w[node][N+1];
+    if (dp[node][visited]) return dp[node][visited];
+    int ret = 2e9;
+    for (int i = 1; i <= N; ++i) {
+        if (visited & (1 << (i - 1))) continue;
+        int t = w[node][i] + travel(i, visited | (1 << (i - 1)));
+        if (t < ret) ret = t;
     }
-    return dist[0][];
+    return dp[node][visited] = ret;
 }
 
 int main() {
-    freopen("input_1247.txt", "r", stdin);
     int T;
     scanf("%d", &T);
     for (int test_case = 1; test_case <= T; ++test_case) {
@@ -35,7 +33,10 @@ int main() {
         for (int i = 0; i <= N; ++i)
             for (int j = i + 1; j <= N + 1; ++j)
                 w[i][j] = w[j][i] = distance(coord[i][0], coord[i][1], coord[j][0], coord[j][1]);
-        printf("#%d %d\n", test_case, travel());
+        for (int i = 0; i < N + 2; ++i)
+            for (int j = 0; j < 1 << N; ++j)
+                dp[i][j] = 0;
+        printf("#%d %d\n", test_case, travel(0, 0));
     }
     return 0;
 }
