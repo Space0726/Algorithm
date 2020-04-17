8598877ae6407e2b4cf8855a7240246823a4e41c
Space0726 <space.sjwoo@gmail.com>
2020-04-17 19:05:12 +0900

    Solved Baekjoon 2108

--- baekjoon/2108.cc
+++ baekjoon/2108.cc
@@ -1,36 +1,32 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 #include <cmath>
-
+#include <algorithm>
 using namespace std;
 
-int n, arr[500000], cnt[8001] = {0,};
-
-int main() {
-    int min_ = 4001, max_ = -4001, sum = 0, freq = 0;
-    bool is_check = false;
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-        if (min_ > arr[i]) min_ = arr[i];
-        if (max_ < arr[i]) max_ = arr[i];
-        sum += arr[i];
-        ++cnt[arr[i] + 4000];
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int N, temp, mean, mode = 0, min_, max_ = 0;
+    bool is_second = false;
+    cin >> N;
+    vector<int> v(N), v2(8001, 0);
+    for(int i = 0; i < N; i++) {
+        cin >> v[i];
+        mean += v[i];
+        temp = v[i] <= 0 ? abs(v[i]) : v[i] + 4000;
+        ++v2[temp];
+        if(v2[temp] > max_) max_ = v2[temp];
     }
-    sort(arr, arr + n);
-    for (int i = min_ + 4000; i <= max_ + 4000; ++i) {
-        if (cnt[i] > cnt[freq]) {
-            freq = i;
-            is_check = false;
-        } else if (cnt[i] == cnt[freq] && !is_check) {
-            freq = i;
-            is_check = true;
+    sort(v.begin(), v.end());
+
+    for(int i = -4000; i < 4001; i++) {
+        temp = i <= 0 ? abs(i) : i + 4000;
+        if(v2[temp] == max_) {
+            mode = i;
+            if(is_second) break;
+            is_second = true;
         }
     }
 
-    float avg = round((float) sum / n);
-    if (-0.1 < avg && avg < 0.1)
-        avg = 0;
-    cout << avg << "\n" <<  arr[n/2] << "\n" <<  freq - 4000 << "\n" << max_ - min_;
-    return 0;
+    cout << round(mean/(double)N) << '\n' << v[round(N/2)] << '\n' << mode << '\n' << v[v.size() - 1] - v[0] << '\n';
 }
