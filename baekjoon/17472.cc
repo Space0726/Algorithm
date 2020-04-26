09fe74abfedddbdb4b93c68dc81fd68a98fa1502
Space0726 <space.sjwoo@gmail.com>
2020-04-27 04:19:42 +0900

    Solved Baekjoon 17472

--- baekjoon/17472.cc
+++ baekjoon/17472.cc
@@ -1,235 +1,165 @@
 #include <iostream>
-#include <vector>
-#include <set>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-struct Edge {
-    pair<int, int> lands;
-    int weight;
-    Edge(pair<int, int> l, int w) {
-        this->lands = l;
-        this->weight = w;
+int N, M, map[10][10], adj[7][7], land_sum = 1;
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+struct Dist {
+    int x, y, dir, cnt, land_num;
+    Dist() {}
+    Dist(int a, int b, int c, int d, int e): x(a), y(b), dir(c), cnt(d), land_num(e) {}
+};
+
+struct LandDist {
+    int from, to, dist;
+    LandDist() {}
+    LandDist(int a, int b, int c): from(a), to(b), dist(c) {}
+    bool operator<(const LandDist& other) const {
+        return dist > other.dist;
     }
 };
 
-struct Land {
-    int num;
-    pair<int, int> tl;
-    pair<int, int> br;
-    Land(int n, pair<int, int> tl_, pair<int, int>br_) {
-        this->num = n;
-        this->tl = tl_;
-        this->br = br_;
+struct Disjoint {
+    int p[7], rank[7];
+    Disjoint() {
+        for (int i = 0; i < land_sum; ++i) {
+            p[i] = i;
+            rank[i] = 0;
+        }
+    }
+    int find_set(int x) {
+        if (x != p[x]) p[x] = find_set(p[x]);
+        return p[x];
+    }
+    void link(int x, int y) {
+        if (rank[x] > rank[y]) {
+            p[y] = x;
+        } else {
+            p[x] = y;
+            if (rank[x] == rank[y])
+                ++rank[y];
+        }
+    }
+    void union_set(int x, int y) {
+        link(find_set(x), find_set(y));
     }
 };
 
-struct LandSet {
-    int num;
-    LandSet *parent;
-    LandSet(int n, LandSet *p) {
-        this->num = n;
-        this->parent = p;
+int kruskal() {
+    Disjoint disjnt;
+    priority_queue<LandDist> pq;
+    int ans = 0;
+    for (int i = 1; i <= land_sum; ++i)
+        for (int j = i + 1; j <= land_sum; ++j)
+            if (adj[i][j] != 2e9) pq.push({i-1, j-1, adj[i][j]});
+    if (pq.empty()) return -1;
+    while (!pq.empty()) {
+        LandDist tmp = pq.top(); pq.pop();
+        int p_from = disjnt.find_set(tmp.from);
+        int p_to = disjnt.find_set(tmp.to);
+        if (p_from != p_to) {
+            disjnt.union_set(tmp.from, tmp.to);
+            ans += tmp.dist;
+        }
     }
-    int find_root() {
-        if (parent == NULL)
-            return num;
-        return parent->find_root();
-    }
-};
-
-int n, m, min_len = 1e4;
-vector<vector<int>> map;
-vector<Land> land_info;
-vector<vector<int>> weights;
-vector<Edge> edges;
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-void print_map() {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j)
-            printf("%d ", map[i][j]);
-        putchar('\n');
-    }
-    putchar('\n');
+    int super_p = disjnt.find_set(0);
+    for (int i = 1; i < land_sum; ++i)
+        if (super_p != disjnt.find_set(i-1)) return -1;
+    return ans;
 }
 
-void print_land_info() {
-    for (Land &l : land_info) {
-        printf("tl: (%d, %d), br: (%d, %d)\n", l.tl.first, l.tl.second, l.br.first, l.br.second);
+void calc_dist(queue<Dist>& q) {
+    int x, y;
+    while (!q.empty()) {
+        Dist tmp = q.front(); q.pop();
+        while (1) {
+            tmp.x += dx[tmp.dir], tmp.y += dy[tmp.dir];
+            if (tmp.x == -1 || tmp.x == N || tmp.y == -1 || tmp.y == M) break;
+            if (map[tmp.x][tmp.y] == tmp.land_num) {
+                break;
+            } else if (map[tmp.x][tmp.y] == 0) { 
+                ++tmp.cnt; continue;
+            } else {
+                if (tmp.cnt != 1 && adj[tmp.land_num][map[tmp.x][tmp.y]] > tmp.cnt)
+                    adj[tmp.land_num][map[tmp.x][tmp.y]] = adj[map[tmp.x][tmp.y]][tmp.land_num] = tmp.cnt;
+                break;
+            }
+        }
     }
-    putchar('\n');
 }
 
-void print_edges() {
-    for (Edge &e : edges) {
-        printf("from: %d, to: %d, weight: %d\n", e.lands.first, e.lands.second, e.weight);
-    }
-    putchar('\n');
-}
-
-void print_weights() {
-    int size = land_info.size();
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j)
-            printf("%d ", weights[i][j]);
-        putchar('\n');
-    }
-    putchar('\n');
-}
-
-void find_lands() {
-    int x, y, xx, yy, cnt = 1;
-    vector<vector<bool>> visited(n, vector<bool>(m));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
+void find_adj() {
+    vector<vector<bool>> visited(N, vector<bool>(M));
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
             if (map[i][j] && !visited[i][j]) {
-                int left = m, top = n, right = -1, bottom = -1;
+                int x, y, xx, yy;
                 queue<pair<int, int>> q;
-                q.push(make_pair(i, j));
+                queue<Dist> q_dist;
+                q.push({i, j});
+                visited[i][j] = true;
                 while (!q.empty()) {
-                    x = q.front().first;
-                    y = q.front().second;
-                    map[x][y] = cnt;
-                    if (left > y)
-                        left = y;
-                    if (top > x)
-                        top = x;
-                    if (right < y)
-                        right = y;
-                    if (bottom < x)
-                        bottom = x;
+                    x = q.front().first, y = q.front().second; q.pop();
                     for (int k = 0; k < 4; ++k) {
-                        xx = x + dx[k];
-                        yy = y + dy[k];
-                        if (xx < 0 || xx >= n || yy < 0 || yy >= m || visited[xx][yy] || !map[xx][yy])
+                        xx = x + dx[k], yy = y + dy[k];
+                        if (xx == -1 || xx == N || yy == -1 || yy == M)
                             continue;
+                        if (visited[xx][yy]) {
+                            if (map[xx][yy] == 0)
+                                q_dist.push({xx, yy, k, 1, map[i][j]});
+                            continue;
+                        }
+                        if (map[xx][yy] == 0)
+                            q_dist.push({xx, yy, k, 1, map[i][j]});
+                        else
+                            q.push({xx, yy});
                         visited[xx][yy] = true;
-                        q.push(make_pair(xx, yy));
                     }
-                    q.pop();
                 }
-                land_info.push_back(Land(cnt, make_pair(top, left), make_pair(bottom, right)));
-                ++cnt;
+                if (!q_dist.empty()) calc_dist(q_dist);
             }
-        }
-    }
 }
 
-void calc_weights() {
-    int size = land_info.size();
-    weights = vector<vector<int>>(size, vector<int>(size));
-    for (int i = 0; i < size; ++i) {
-        Land cur = land_info[i];
-        int cur_num = cur.num - 1;
-        for (int k = 0; k < 4; ++k) {
-            switch (k) {
-                case 0:
-                    for (int y = cur.tl.second; y <= cur.br.second; ++y) {
-                        for (int x = cur.br.first + 1; x < n; ++x) {
-                            if (map[x][y]) {
-                                if (!weights[cur_num][map[x][y] - 1]) {
-                                    if (x - cur.br.first - 1 >= 2) {
-                                        weights[cur_num][map[x][y] - 1] = weights[map[x][y] - 1][cur_num] = x - cur.br.first - 1;
-                                        edges.push_back(Edge(make_pair(cur_num, map[x][y] - 1), x - cur.br.first - 1));
-                                    }
-                                }
-                                break;
-                            }
-                        }
+void add_land_num() {
+    vector<vector<bool>> visited(N, vector<bool>(M));
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
+            if (map[i][j] && !visited[i][j]) {
+                int x, y, xx, yy;
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                visited[i][j] = true;
+                map[i][j] = land_sum;
+                while (!q.empty()) {
+                    x = q.front().first, y = q.front().second; q.pop();
+                    for (int k = 0; k < 4; ++k) {
+                        xx = x + dx[k], yy = y + dy[k];
+                        if (xx == -1 || xx == N || yy == -1 || yy == M || visited[xx][yy] || !map[xx][yy])
+                            continue;
+                        q.push({xx, yy});
+                        map[xx][yy] = land_sum;
+                        visited[xx][yy] = true;
                     }
-                    break;
-                case 1:
-                    for (int y = cur.tl.second; y <= cur.br.second; ++y) {
-                        for (int x = cur.tl.first - 1; x >= 0; --x) {
-                            if (map[x][y]) {
-                                if (!weights[cur_num][map[x][y] - 1]) {
-                                    if (cur.tl.first - x - 1 >= 2) {
-                                        weights[cur_num][map[x][y] - 1] = weights[map[x][y] - 1][cur_num] = cur.tl.first - x - 1;
-                                        edges.push_back(Edge(make_pair(cur_num, map[x][y] - 1), cur.tl.first - x - 1));
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                case 2:
-                    for (int x = cur.tl.first; x <= cur.br.first; ++x) {
-                        for (int y = cur.br.second + 1; y < m; ++y) {
-                            if (map[x][y]) {
-                                if (!weights[cur_num][map[x][y] - 1]) {
-                                    if (y - cur.br.second - 1 >= 2) {
-                                        weights[cur_num][map[x][y] - 1] = weights[map[x][y] - 1][cur_num] = y - cur.br.second - 1;
-                                        edges.push_back(Edge(make_pair(cur_num, map[x][y] - 1), y - cur.br.second - 1));
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                case 3:
-                    for (int x = cur.tl.first; x <= cur.br.first; ++x) {
-                        for (int y = cur.tl.second - 1; y >= 0; --y) {
-                            if (map[x][y]) {
-                                if (!weights[cur_num][map[x][y] - 1]) {
-                                    if (cur.tl.second - y - 1 >= 2) {
-                                        weights[cur_num][map[x][y] - 1] = weights[map[x][y] - 1][cur_num] = cur.tl.second - y - 1;
-                                        edges.push_back(Edge(make_pair(cur_num, map[x][y] - 1), cur.tl.second - y - 1));
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
+                }
+                ++land_sum;
             }
-        }
-    }
-}
-
-void kruskal() {
-    sort(edges.begin(), edges.end(), [](auto &a, auto &b) { return a.weight < b.weight; });
-    vector<LandSet> land_set;
-    int land_size = land_info.size(), cnt = 1;
-    bool success = false;
-    for (int i = 0; i < land_size; ++i)
-        land_set.push_back(LandSet(i, NULL));
-    int sum = 0;
-    for (int i = 0; i < edges.size(); ++i) {
-        int from = land_set[edges[i].lands.first].find_root();
-        int to = land_set[edges[i].lands.second].find_root();
-        if (from == to)
-            continue;
-        else {
-            sum += edges[i].weight;
-            land_set[to].parent = &land_set[from];
-            ++cnt;
-        }
-        if (cnt == land_size) {
-            success = true;
-            break;
-        }
-    }
-    if (!success)
-        min_len = -1;
-    else if (min_len > sum)
-        min_len = sum;
+    --land_sum;
 }
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    scanf("%d %d", &n, &m);
-    map = vector<vector<int>>(n, vector<int>(m));
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
+    scanf("%d%d", &N, &M);
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < M; ++j)
             scanf("%d", &map[i][j]);
-    find_lands();
-    calc_weights();
-    kruskal();
-    printf("%d", min_len);
+    for (int i = 1; i < 7; ++i)
+        for (int j = 1; j < 7; ++j)
+            adj[i][j] = 2e9;
+    add_land_num();
+    find_adj();
+    printf("%d", kruskal());
+    return 0;
 }
